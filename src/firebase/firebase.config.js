bf87af59cd6c52891cb1b36444d41d37e457e133
Tokiosman80F <tokiosman96@gmail.com>
2023-09-11 01:11:14 +0600

    file structure is ready

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgZgBPgty8rDH3qzzTVcVQqtCO5Whm428",
+  authDomain: "travelwise-ba408.firebaseapp.com",
+  projectId: "travelwise-ba408",
+  storageBucket: "travelwise-ba408.appspot.com",
+  messagingSenderId: "925878845262",
+  appId: "1:925878845262:web:aede1a096ea1014911281f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
