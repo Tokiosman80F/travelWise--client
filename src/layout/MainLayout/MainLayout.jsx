c30cc0e8add0bf002ad1cd7f9e90185f32c49984
Tokiosman80F <tokiosman96@gmail.com>
2023-09-11 10:54:10 +0600

    navbar is created

---
+++ src/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+import { Container } from "react-bootstrap"
+import NavigationBar from "../../pages/shared/NavigationBar"
+
+const MainLayout = () => {
+    return (
+        <Container>
+            <NavigationBar></NavigationBar>
+        </Container>
+    )
+}
+
+export default MainLayout
---
+++ src/pages/shared/NavigationBar.jsx
@@ -0,0 +1,33 @@
+// import React from 'react'
+import { Button, Container, Form, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+const NavigationBar = () => {
+    return (
+      <div>
+        <Navbar expand='lg' className='bg-body-tertiary'>
+          <Container fluid>
+            <Navbar.Brand href='#'><img src={logo} alt="" width={150} /></Navbar.Brand>
+            <Navbar.Toggle aria-controls='navbarScroll' />
+            <Navbar.Collapse id='navbarScroll'>
+              <Nav
+                className='mx-auto my-2 my-lg-0 d-flex gap-2'
+                style={{ maxHeight: "150px" }}
+                navbarScroll>
+                <Link className='text-decoration-none text-secondary '>Home</Link>
+                <Link className='text-decoration-none text-secondary  '>Destination</Link>
+                <Link className='text-decoration-none text-secondary '>Blogs</Link>
+                <Link className='text-decoration-none text-secondary '>Contact</Link>
+              </Nav>
+              <Form className='d-flex'>
+                
+                <Button variant='warning'>Login</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+}
+
+export default NavigationBar
