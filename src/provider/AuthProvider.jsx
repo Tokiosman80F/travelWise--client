f06593b306d78f30310b98595798d1724d6c56d5
Tokiosman80F <tokiosman96@gmail.com>
2023-09-13 16:27:07 +0600

    signout and observer function is add in authProvider component

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,13 +1,14 @@
-import { createContext } from 'react'
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from 'react'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
 
 export const AuthContext=createContext(null);
 
 
+const auth = getAuth(app);
 const AuthProvider = ({children}) => {
-    const auth = getAuth(app);
-    const user={displayName:"toki"}
+    
+    const [user,setUser]=useState(null)
     // signup user 
     const createUser=(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password)
@@ -16,7 +17,18 @@ const AuthProvider = ({children}) => {
     const loginUser=(email,password)=>{
         return signInWithEmailAndPassword(auth,email,password)
     }
-    const authInfo={user,createUser,loginUser}
+    // signOut
+    const logOut=()=>{
+        return signOut(auth);
+    }
+    // observer
+    useEffect(()=>{
+       const unSubscribe= onAuthStateChanged(auth,(loggedUser)=>{
+        console.log("loggedUser",loggedUser);
+        setUser(loggedUser)})
+        return ()=>{unSubscribe()}
+        },[])
+    const authInfo={user,createUser,loginUser,logOut}
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
